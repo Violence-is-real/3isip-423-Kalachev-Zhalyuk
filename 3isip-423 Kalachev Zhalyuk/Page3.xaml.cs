5ae9e0b1b97ef7761cc0c0760c905757c970db3f
Жалюк П.В. <zhalyuk.polina1@gmail.com>
2026-03-02 10:19:38 +0300

    добавлен функционал третьей страницы

--- 3isip-423 Kalachev Zhalyuk/Page3.xaml.cs
+++ 3isip-423 Kalachev Zhalyuk/Page3.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace _3isip_423_Kalachev_Zhalyuk
 {
@@ -23,6 +14,70 @@ namespace _3isip_423_Kalachev_Zhalyuk
         public Page3()
         {
             InitializeComponent();
+            ChartFunction.Titles.Add("График функции");
+            ChartFunction.ChartAreas.Add(new ChartArea("Main"));
+            var currentSeries = new Series("y")
+            {
+                IsValueShownAsLabel = false
+            };
+            ChartFunction.Series.Add(currentSeries);
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            double x0, xk, dx, b;
+            if (!double.TryParse(tbX0.Text, out x0) ||
+                !double.TryParse(tbXk.Text, out xk) ||
+                !double.TryParse(tbDx.Text, out dx) ||
+                !double.TryParse(tbB.Text, out b) ||
+                dx == 0)
+            {
+                MessageBox.Show("Введите корректные числовые значения! dx != 0");
+                return;
+            }
+
+            if (x0 > xk && dx > 0 || x0 < xk && dx < 0)
+            {
+                MessageBox.Show("Неправильное направление шага!");
+                return;
+            }
+
+            List<KeyValuePair<double, double>> data = new List<KeyValuePair<double, double>>();
+            string results = "";
+            for (double x = x0; dx > 0 ? x <= xk : x >= xk; x += dx)
+            {
+                double y;
+                try
+                {
+                    y = 9 * (Math.Pow(x, 3) + Math.Pow(b, 3)) * Math.Tan(x);
+                }
+                catch
+                {
+                    MessageBox.Show($"Ошибка вычисления при x={x}, возможно тангенс неопределен!");
+                    return;
+                }
+                results += $"x={x}, y={y}\n";
+                data.Add(new KeyValuePair<double, double>(x, y));
+            }
+            tbResults.Text = results;
+
+            Series currentSeries = ChartFunction.Series[0];
+            currentSeries.ChartType = SeriesChartType.Line;
+            currentSeries.Points.Clear();
+            foreach (var point in data)
+            {
+                currentSeries.Points.AddXY(point.Key, point.Value);
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            tbX0.Text = "";
+            tbXk.Text = "";
+            tbDx.Text = "";
+            tbB.Text = "";
+            tbResults.Text = "";
+            ChartFunction.Series[0].Points.Clear();
         }
     }
 }
